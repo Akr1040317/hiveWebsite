1dd19c1bf7cdb7584dc2b9c051468417fdafa96d
Akshat Rastogi <akshra0317@gmail.com>
2024-10-16 19:20:20 -0400

    changes to new category button

--- src/LessonManagement.jsx
+++ src/LessonManagement.jsx
@@ -1317,7 +1317,7 @@ const populateForm = (lesson) => {
               <div className="mb-4">
                 <button
                   type="button"
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 flex items-center"
+                  className="bg-[#ffa500] text-white px-4 py-2 rounded-lg hover:bg-blue-600 flex items-center"
                   onClick={() => setShowNewCategoryModal(true)}
                 >
                   <FaPencilAlt className="mr-2" /> New Category
